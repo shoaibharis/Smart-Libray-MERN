01f6ed5c21ff86d07b64c0196c927f09fdff0a9e
Shoaib Haris <numl-f20-13983@numls.edu.pk>
2023-07-07 19:55:58 +0500

    Delete backend directory

--- backend/app.js
+++
@@ -1,8 +0,0 @@
-const express=require("express")
-const app=express();
-const employeeRoutes=require("./Routes/employeeRoutes")
-app.use(express.json)
-
-app.use("/api/v2",employeeRoutes);
-
-module.exports=app
